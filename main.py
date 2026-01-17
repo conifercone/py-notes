29b3d6f013a99eca347d55035bd016e33d609ecd
Kaiyu Shan <kaiyu.shan@outlook.com>
2026-01-17 12:41:38 +0800

    feat(main): 测试uuid

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import uuid
+
 def main():
     print("Hello from py-notes!")
     print("测试代码")
+    userid = uuid.uuid6()
+    print(userid)
+    useridV4 = uuid.uuid4()
+    print(useridV4)
 
 
 if __name__ == "__main__":
